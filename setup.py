a78ce2384640bb701a1a21c754342a0f3b00f1f4
Frederick-S <mao_xiaodan@hotmail.com>
2018-02-27 14:50:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='mypc',
     version='0.0.1',
-    description='Open "This Computer" on Windows.',
+    description='Open "This PC" on Windows.',
     url='https://github.com/Frederick-S/mypc',
     packages=find_packages(exclude=['tests']),
     entry_points={
